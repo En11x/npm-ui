1383976dc75ff18330cc34cf59ba77708712f3ac
Xin <497421855@qq.com>
2022-11-13 13:39:25 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import VueTermui from "vite-plugin-vue-termui";
+import pkg from "./package.json";
+
+const deps = Object.keys(pkg.dependencies);
 
 export default defineConfig({
   plugins: [VueTermui()],
@@ -10,5 +13,10 @@ export default defineConfig({
       process.env.NODE_ENV === "production"
         ? ["module", "main"]
         : ["main", "module"],
+  },
+  build: {
+    rollupOptions: {
+      external: deps,
+    },
   },
 });
